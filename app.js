6fb9be20635d23a81201e53a4ebdb7000d47f1cf
Tien Pham <r3dsky02@yahoo.com.vn>
2018-12-28 22:18:08 +0200

    Adding Relations

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser")
 const graphqlHttp = require("express-graphql")
 const { buildSchema } = require("graphql")
 const mongoose = require("mongoose")
+const bcrypt = require('bcryptjs');
 
 const Event = require("./models/event")
+const User = require("./models/user")
 
 const app = express()
 
@@ -22,6 +24,12 @@ app.use(
       date: String!
     }
 
+    type User {
+      _id: ID!
+      email: String!
+      password: String
+    }
+
     type RootQuery {
       events: [Event!]!
     }
@@ -32,9 +40,15 @@ app.use(
       price: Float!
       date: String!
     }
+
+    input UserInput {
+      email: String!
+      password: String!
+    }
     
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
     }
 
     schema {
@@ -47,12 +61,12 @@ app.use(
         return Event.find()
           .then(events => {
             return events.map(event => {
-              return { ...event._doc, _id: event.id };
-            });
+              return { ...event._doc, _id: event.id }
+            })
           })
           .catch(err => {
-            throw err;
-          });
+            throw err
+          })
       },
       createEvent: args => {
         const event = new Event({
@@ -71,7 +85,28 @@ app.use(
             console.log(err)
             throw err
           })
-        return event
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error("User exists already.")
+            }
+            return bcrypt.hash(args.userInput.password, 12)
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            })
+            return user.save()
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id }
+          })
+          .catch(err => {
+            throw err
+          })
       }
     },
     graphiql: true
--- models/event.js
+++ models/event.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
   title: {
@@ -18,7 +18,11 @@ const eventSchema = new Schema({
   date: {
     type: Date,
     required: true
+  },
+  creator: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
   }
-})
+});
 
-module.exports = mongoose.model("Event", eventSchema)
+module.exports = mongoose.model('Event', eventSchema);
